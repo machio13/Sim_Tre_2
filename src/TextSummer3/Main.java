08e77667c90e5cd0c40dbaf76acc027f736b6244
machio13 <o8078281124@gmail.com>
2024-10-22 20:20:48 +0900

    ステップ１　3周目

---
+++ src/TextSummer3/Main.java
@@ -0,0 +1,9 @@
+package TextSummer3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+        menu.MenuSelect();
+    }
+}
---
+++ src/TextSummer3/Menu.java
@@ -0,0 +1,51 @@
+package TextSummer3;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public void MenuSelect() {
+        System.out.println("株式管理システムを開始します。");
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isRunning = true;
+        while (isRunning){
+            System.out.println("操作するメニューを選択してください。");
+            System.out.println("1. 銘柄マスタ一覧表示");
+            System.out.println("2. 銘柄マスタ新規登録");
+            System.out.println("3. 取引入力");
+            System.out.println("4. 取引表示");
+            System.out.println("9. アプリケーションを終了する");
+            System.out.print("入力してください：");
+            String userInput = scanner.nextLine();
+
+            switch (userInput) {
+                case "1" -> {
+                    System.out.println("「銘柄マスタ一覧表示」が選択されました。");
+                    System.out.println("---");
+                }
+                case "2" -> {
+                    System.out.println("「銘柄マスタ新規登録」が選択されました。");
+                    System.out.println("---");
+                }
+                case "3" -> {
+                    System.out.println("「取引入力」が選択されました。");
+                    System.out.println("---");
+                }
+                case "4" -> {
+                    System.out.println("「取引表示」が選択されました。");
+                    System.out.println("---");
+                }
+                case "9" -> {
+                    System.out.println("アプリケーションを終了します。");
+                    System.out.println("---");
+                    isRunning = false;
+                }
+                default -> {
+                    System.out.println("\"" + userInput + "\"に対応するメニューは存在しません。");
+                    System.out.println("---");
+                }
+            }
+        }
+    }
+}
