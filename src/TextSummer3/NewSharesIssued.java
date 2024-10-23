930ede38542b5e20991a5ca5821d9e64c13eeaba
machio13 <o8078281124@gmail.com>
2024-10-23 11:45:14 +0900

    ステップ3 途中経過2　3周目

--- src/TextSummer3/NewSharesIssued.java
+++ src/TextSummer3/NewSharesIssued.java
@@ -1,4 +1,34 @@
 package TextSummer3;
 
+import javax.swing.plaf.ScrollBarUI;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class NewSharesIssued {
+    private BigDecimal newSharesIssued;
+
+    public void addSharesIssued() {
+        Scanner scanner = new Scanner(System.in);
+        boolean check = true;
+
+        while(check) {
+            System.out.print("発行済み株式数>");
+            try {
+                String userInput = scanner.nextLine();
+                if (userInput.length() < 13) {
+                    newSharesIssued = new BigDecimal(userInput);
+                    check = false;
+                }else {
+                    System.out.println("12桁を超えた入力です。入力し直してください。");
+                }
+            }catch (NumberFormatException e) {
+                System.out.println("数字を入力してください。");
+            }
+        }
+    }
+
+    public BigDecimal getNewSharesIssued() {
+        return newSharesIssued;
+    }
 }
